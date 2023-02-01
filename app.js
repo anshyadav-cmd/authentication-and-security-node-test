6358e3ef6744c553506ebec195166eff2d021b9a
anshyadav <anshyadav4244@gmail.com>
2023-02-01 22:40:01 +0530

    level 3 Hashing
    there are loads of precomplied hash pass table so make your password long for better security

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express")
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -22,8 +22,6 @@ const userSchema = new mongoose.Schema({
 // encryption 
 const secret = process.env.SECRET;
 
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
-
 const User = mongoose.model("User", userSchema);
 
 app.get("/", function(req, res) {
@@ -35,7 +33,7 @@ app.get("/login", function(req, res) {
 
 app.post("/login", function(req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, foundUser){
         if(!err){
@@ -61,7 +59,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
     const user = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     user.save(function(err) {
         if(!err){
