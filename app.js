bcd3aea8b29add2eacb8bd3e8c5852d1d88e4cfd
anshyadav <anshyadav4244@gmail.com>
2023-02-02 16:34:17 +0530

    finished project

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ mongoose.connect("mongodb://localhost:27017/userDB");
 const userSchema = new mongoose.Schema({
     email : String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 })
 
 userSchema.plugin(passportLocalMongoose);
@@ -118,6 +119,33 @@ app.post("/register", function(req, res) {
    })
 });
 
+app.get("/submit", function(req, res) {
+    if(req.isAuthenticated()){
+        res.render("submit");
+    }else {
+        res.redirect("/login");
+    }
+})
+
+app.post("/submit", function(req, res) {
+    const submittedSecret = req.body.secret;
+
+    User.findById(req.user.id, function(err, foundUser) {
+        if(err) {
+            console.log(err);
+        }else {
+            if(foundUser){
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(err) {
+                    if(!err){
+                        res.redirect("/secrets");
+                    }
+                });
+            }
+        }
+    })
+})
+
 app.get("/logout", function(req, res) {
     req.logout(function(err) {
         if(!err) {
@@ -129,12 +157,16 @@ app.get("/logout", function(req, res) {
 })
 
 app.get("/secrets", function(req, res) {
-    if(req.isAuthenticated()){
-        res.render("secrets");
-    }else {
-        res.redirect("/login");
-    }
-})
+    User.find({"secret": {$ne: null}}, function(err, foundUsers) {
+        if(err) {
+            console.log(err);
+        }else {
+            if(foundUsers){
+                res.render("secrets", {usersWithSecrets: foundUsers});
+            }
+        }
+    });
+});
 
 app.listen(3000, function() {
     console.log("server started on port 3000");
